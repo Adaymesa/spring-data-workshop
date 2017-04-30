01690a0bbe4638b793add0d480dc8261b0d4c3e6
Angel Villalaín <angel.villalain@gmail.com>
2017-04-30 23:14:26 +0200

    Custom Repository method

--- src/main/java/com/thoughtworks/jpa/repository/ProfessorRepository.java
+++ src/main/java/com/thoughtworks/jpa/repository/ProfessorRepository.java
@@ -4,5 +4,9 @@ import com.thoughtworks.jpa.domain.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProfessorRepository extends JpaRepository<Professor, Long>{ }
+public interface ProfessorRepository extends JpaRepository<Professor, Long>{
+    List<Professor> findByLastNameIgnoreCaseOrderById(String lastName);
+}
--- src/test/java/com/thoughtworks/jpa/repository/ProfessorRepositoryTest.java
+++ src/test/java/com/thoughtworks/jpa/repository/ProfessorRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.thoughtworks.jpa.repository;
 
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.thoughtworks.jpa.domain.Professor;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 import static com.thoughtworks.jpa.domain.Professor.Type.ADJUNCT;
 import static java.time.LocalDate.of;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProfessorRepositoryTest extends BaseRepositoryTest {
 
@@ -19,6 +23,15 @@ public class ProfessorRepositoryTest extends BaseRepositoryTest {
         Professor professor = new Professor("identification", "First Name",
                 "Last Name", of(1983, 01, 03), ADJUNCT);
         repository.save(professor);
+    }
+
+    @Test
+    @DatabaseSetup(value = "classpath:datasets/professor/shouldFindProfessorByLastNameIgnoreCase.xml")
+    public void shouldFindProfessorByLastNameIgnoreCase() {
+        List<Professor> professors = repository.findByLastNameIgnoreCaseOrderById("snape");
+        assertThat(professors).isNotEmpty().hasSize(2);
+        assertThat(professors.get(0).getId()).isEqualTo(2);
+        assertThat(professors.get(1).getId()).isEqualTo(3);
     }
 
 }
